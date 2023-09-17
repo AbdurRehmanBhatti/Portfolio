f4cbeceb1b3ba3683ee391e80b8fff2c19d18678
AbdurRehman <abdurrehman1a3@gmail.com>
2023-09-17 15:12:02 +0500

    To be Continued (After The Test)

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ var typed = new Typed(".text", {
     backDelay: 1000,
     loop: true
 });
+
+// Get elements
+const progressBar = document.querySelector('.progress-bar');
+const percentageText = document.querySelector('.percentage');
+const percentageInput = document.getElementById('percentageInput');
+
+// Update progress bar and text based on input value
+percentageInput.addEventListener('input', () => {
+  const percentage = percentageInput.value;
+  progressBar.style.clip = `rect(0px, ${percentage}px, 150px, 0px)`;
+  percentageText.textContent = `${percentage}%`;
+});
